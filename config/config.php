3ab081f511f866f22cef88c5e8f4b55ab5a4b7b7
coco <oscargonz150@gmail.com>
2022-08-24 18:17:16 -0600

    first commit

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php 
+
+    define('PATH','http://localhost/Real/');
+
+    $data_base_config = array(
+        'host' => 'localhost',
+        'data_base' => 'crud',
+        'user' => 'root',
+        'password' => ''
+    );
+    
+?>
